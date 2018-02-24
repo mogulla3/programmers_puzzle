bfacb53de2a61fcf20a51fb6a9a85183d73168fa
mogulla3 <hs.iarn3@gmail.com>
2018-02-25 00:16:23 +0900

    Add q01.rb

---
+++ v2/q01.rb
@@ -0,0 +1,15 @@
+NUM = 100
+cnt = 0
+
+0.upto(NUM) do |guu|
+  0.upto(NUM) do |choki|
+    0.upto(NUM) do |paa|
+      next unless guu + choki + paa == NUM
+      arr = [guu, choki, paa].sort.reverse
+      next if arr[0] == arr[1]
+      cnt += 1
+    end
+  end
+end
+
+puts cnt
